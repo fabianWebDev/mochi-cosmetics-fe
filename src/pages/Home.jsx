c8f15cc8e3e310a9065c1d9fdd3169ddc9e59f00
fabianWebDev <fabiancava22@gmail.com>
2025-03-12 22:36:44 -0600

    Added provisional nav bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
-    return <>
-        <div>Hello world</div>
-    </>
+    return (
+        <>
+            <nav className="bg-gray-800 p-4">
+                <div className="container mx-auto flex justify-between items-center">
+                    <Link to="/" className="text-white font-bold text-xl">
+                        Mi Tienda
+                    </Link>
+                    <div className="flex space-x-4">
+                        <Link to="/" className="text-white hover:text-gray-300">
+                            Inicio
+                        </Link>
+                        <Link to="/productos" className="text-white hover:text-gray-300">
+                            Productos
+                        </Link>
+                        <Link to="/carrito" className="text-white hover:text-gray-300">
+                            Carrito
+                        </Link>
+                        <Link to="/contacto" className="text-white hover:text-gray-300">
+                            Contacto
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+            <div className="container mx-auto mt-8">
+                <h1 className="text-3xl font-bold">Bienvenido a Mi Tienda</h1>
+            </div>
+        </>
+    );
 }
 
 export default Home; 
