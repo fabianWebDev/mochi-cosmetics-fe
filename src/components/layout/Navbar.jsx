690156e6c25aae9bc864d1809e17db5dbb4d24bf
fabianWebDev <fabiancava22@gmail.com>
2025-03-23 23:31:57 -0600

    Navbar basic structure

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,6 +4,8 @@ import SearchBar from '../ui/SearchBar';
 import { useEffect, useState } from 'react';
 import { productService } from '../../services/productService';
 import { MEDIA_BASE_URL } from '../../constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
     const navigate = useNavigate();
@@ -34,78 +36,95 @@ function Navbar() {
     }, []);
 
     return (
-        <nav>
-            <div>
-                <Link to="/">
-                    My Store
-                </Link>
-                <div>
+        <div className="container">
+            <div className="row justify-content-between">
+                <div className="col">
                     <Link to="/">
-                        Home
+                        My Store
                     </Link>
-                    <div
-                        className="relative"
-                        onMouseEnter={() => setShowSubMenu(true)}
-                        onMouseLeave={() => setShowSubMenu(false)}
-                    >
-                        <Link to="/products">
-                            Products
-                        </Link>
-                        {showSubMenu && (
-                            <div>
-                                {categories.map(category => (
-                                    <Link
-                                        key={category.id}
-                                        to={`/products?search=${category.name}`}
-                                    >
-                                        {category.name}
-                                    </Link>
-                                ))}
-                            </div>
-                        )}
-                    </div>
-                    <Link to="/contact">
-                        Contact
-                    </Link>
+                </div>
+                <div className="col">
                     <Link to="/cart">
-                        Cart
-                    </Link>
-                    <Link to="/about">
-                        About
+                        <FontAwesomeIcon icon={faCartShopping} style={{ color: '#eb9ec1' }} />
                     </Link>
                     {!user ? (
-                        <>
-                            <Link to="/login">
-                                Login
-                            </Link>
-                            <Link to="/register">
-                                Register
-                            </Link>
-                        </>
+                        <Link to="/login">
+                            <FontAwesomeIcon icon={faUser} style={{ color: '#eb9ec1' }} />
+                        </Link>
                     ) : (
-                        <>
-                            <Link to="/orders">
-                                My Orders
+                        <button onClick={handleLogout}>
+                            Logout
+                        </button>
+                    )}
+                </div>
+            </div>
+            <div className="row">
+                <nav className="navbar">
+                    <div className="container-fluid">
+                        <Link to="/products?search=mtg">
+                            Magic The Gathering
+                        </Link>
+                        <div
+                            className="relative"
+                            onMouseEnter={() => setShowSubMenu(true)}
+                            onMouseLeave={() => setShowSubMenu(false)}
+                        >
+                            <Link to="/products">
+                                Products
                             </Link>
-                            {user.is_admin && (
-                                <Link to={`${MEDIA_BASE_URL}/admin`}>
-                                    Admin Panel
+                        </div>
+                        <Link to="/contact">
+                            Contact
+                        </Link>
+
+                        <Link to="/about">
+                            About
+                        </Link>
+                        {!user ? (
+                            <>
+
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/orders">
+                                    My Orders
                                 </Link>
-                            )}
-                            <span>
-                                {user.first_name || user.username}
-                            </span>
-                            <button
-                                onClick={handleLogout}
-                            >
-                                Logout
-                            </button>
+                                {user.is_admin && (
+                                    <Link to={`${MEDIA_BASE_URL}/admin`}>
+                                        Admin Panel
+                                    </Link>
+                                )}
+                                <span>
+                                    {user.first_name || user.username}
+                                </span>
+
+                            </>
+                        )}
+                    </div>
+                </nav>
+            </div>
+            <div className="row">
+                <div className="container-fluid">
+                    {showSubMenu && (
+                        <>
+                            {categories.map(category => (
+                                <Link
+                                    key={category.id}
+                                    to={`/products?search=${category.name}`}
+                                >
+                                    {category.name}
+                                </Link>
+                            ))}
                         </>
                     )}
                 </div>
             </div>
-            <SearchBar />
-        </nav>
+            <div className="row">
+                <div className="">
+                    <SearchBar />
+                </div>
+            </div>
+        </div >
     );
 }
 
