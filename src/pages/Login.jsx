47e63681a4c8c434fdb928f48b604eec167086f9
fabianWebDev <fabiancava22@gmail.com>
2025-04-07 14:13:39 -0600

    Login styles update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,9 +15,8 @@ const Login = () => {
     };
 
     return (
-        <div className="login-container">
-            <div className="login-form-wrapper">
-                <h2>Login</h2>
+        <div className="row justify-content-center mt-4">
+            <div className="col-md-4 col-sm-6">
                 <LoginForm
                     formData={formData}
                     onChange={handleChange}
