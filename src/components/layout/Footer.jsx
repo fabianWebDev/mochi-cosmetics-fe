071ab7d36123ea5e9881923ba70c44613fdc4904
fabianWebDev <fabiancava22@gmail.com>
2025-04-09 01:22:32 -0600

    Best practices update

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <footer className={`${classes.footer_container}`}>
             <div className={`row mt-5 ${classes.footer_row}`}>
-                <hr className={`${classes.footer_hr} mb-2`}/>  
+                <hr className={`${classes.footer_hr} mb-2`} />
                 <div className={`col-md-4 ${classes.footer_col}`}>
                     <FooterMenu className={classes.footer_menu} />
                 </div>
@@ -18,7 +18,7 @@ const Footer = () => {
                     <FooterSocial className={classes.footer_menu} />
                 </div>
             </div>
-            <hr className={`${classes.footer_hr} mt-2`}/>
+            <hr className={`${classes.footer_hr} mt-2`} />
             <p className={`${classes.footer_copyright} text-center mt-2 mb-2`}>
                 &copy; {new Date().getFullYear()} Todos los derechos reservados.
             </p>
