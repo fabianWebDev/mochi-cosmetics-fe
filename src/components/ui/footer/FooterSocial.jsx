5bd6a125ffaadb27999b774b3521759fb2b7ae37
fabianWebDev <fabiancava22@gmail.com>
2025-04-16 18:47:02 -0600

    Footer menues update

--- src/components/ui/footer/FooterSocial.jsx
+++ src/components/ui/footer/FooterSocial.jsx
@@ -1,17 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 const FooterSocial = ({ className }) => {
     return (
         <div className={className}>
             <h3>Follow us</h3>
             <div className="social-media justify-content-center">
-                <a href="/">
+                <Link to="https://www.facebook.com/profile.php?id=61552798884871" target="_blank">
                     <FontAwesomeIcon icon={faFacebook} style={{ width: '40px', height: '40px', color: 'var(--primary-color)' }} />
-                </a>
-                <a href="/">
+                </Link>
+                <Link to="https://www.instagram.com/minas_morgul_cards/" target="_blank">
                     <FontAwesomeIcon icon={faInstagram} style={{ width: '40px', height: '40px', color: 'var(--primary-color)' }} />
-                </a>
+                </Link>
             </div>
         </div>
     )
