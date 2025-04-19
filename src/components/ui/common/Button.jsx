fd04e4aff33a83974cf2808bdd07728059273c76
fabianWebDev <fabiancava22@gmail.com>
2025-04-18 19:28:03 -0600

    Global button

---
+++ src/components/ui/common/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ children, onClick, className, disabled }) => {
+    return (
+        <button onClick={onClick} className={className} disabled={disabled}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
