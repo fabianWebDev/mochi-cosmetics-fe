aa0b123349048de025a2ba8fccd3778aa1ba6157
fabianWebDev <fabiancava22@gmail.com>
2025-04-25 00:33:43 -0600

    Cart styles update

--- src/components/cart/CartSummary.jsx
+++ src/components/cart/CartSummary.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import classes from './CartSummary.module.css';
 
 const CartSummary = ({ total, onCheckout, isAuthenticated }) => (
-    <div className="d-flex justify-content-between align-items-center">
+    <div className="d-flex justify-content-between align-items-center mt-2">
         <h4>Total: ${total.toFixed(2)}</h4>
-        <button className={`${classes.cart_summary_button} mt-2`} onClick={onCheckout}>
+        <button className={`${classes.cart_summary_button}`} onClick={onCheckout}>
             {isAuthenticated ? 'Proceed to Checkout' : 'Sign in to Checkout'}
         </button>
     </div>
