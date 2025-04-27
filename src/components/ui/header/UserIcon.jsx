3bd741530de3f59ec082b17b0abdc02127def479
fabianWebDev <fabiancava22@gmail.com>
2025-04-26 20:19:10 -0600

    User options dropdown menu

--- src/components/ui/header/UserIcon.jsx
+++ src/components/ui/header/UserIcon.jsx
@@ -1,11 +1,21 @@
 import { FiUser } from "react-icons/fi";
+import { useState } from "react";
+import UserDropdown from "./UserDropdown";
+import classes from "./HeaderIcons.module.css";
 
-const UserIcon = ({ onClick, className }) => {
-  return (
-    <div className={className}>
-      <FiUser />
-    </div>
-  );
+const UserIcon = ({ className }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    return (
+        <div 
+            className={`${className} ${classes.user_icon_container}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <FiUser />
+            {isHovered && <UserDropdown />}
+        </div>
+    );
 };
 
 export default UserIcon;
