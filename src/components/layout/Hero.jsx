b5637f2c8bdb45c25fa6b7c64df38a1b9e28f23b
fabianWebDev <fabiancava22@gmail.com>
2025-05-05 16:40:45 -0600

    Hero update

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -3,10 +3,10 @@ import classes from "./Hero.module.css";
 const Hero = () => {
     return (
         <div className={`${classes.hero_container} mt-4`}>
-            <div className={`${classes.hero_title_container} row`}>
-                <h1 className={classes.hero_title}>Welcome to Minas Morgul Cards, your one stop shop for all your card needs!</h1>
+            <div className="row">
+                <h1 className={classes.hero_title}>Enter the Shadows of Minas Morgul — Where Darkness Deals the Cards.</h1>
             </div>
-            <div className={`row`}>
+            <div className="row mt-3">
                 <div className="col"></div>
                 <div className={`${classes.hero_button_container} col-md-6`}>
                     <button className={classes.hero_button}>
