5535c7337023cf4ef894dcee43bac3cfffd26121
fabianWebDev <fabiancava22@gmail.com>
2025-06-03 15:14:42 -0600

    UI update

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -4,7 +4,7 @@ import { MEDIA_BASE_URL } from '../../constants';
 
 const ProductList = ({ products, handleViewDetails, handleAddToCart, addingToCart }) => {
     return (
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 mt-3 mb-4">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 mt-3">
             {products.map((product) => (
                 <div key={product.id} className="col mb-2 px-1">
                     <Card
