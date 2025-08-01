5d9eb013f494db2f6ab676ac29947130c92034b5
fabianWebDev <fabiancava22@gmail.com>
2025-08-01 14:14:27 -0600

    404 page

--- src/components/ui/common/index.js
+++ src/components/ui/common/index.js
@@ -2,3 +2,4 @@ export { default as Toast } from './Toast';
 export { default as ProgressBar } from './ProgressBar';
 export { default as Pagination } from './Pagination';
 export { default as Loading } from './Loading'; 
+export { default as Button } from './Button';
