d8ba5271db326f9bdcdc6c52aa4c8629fa00c0ec
fabianWebDev <fabiancava22@gmail.com>
2025-09-11 13:42:39 -0600

    Styles fix 1

--- src/components/categories/CategoryCard.jsx
+++ src/components/categories/CategoryCard.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import classes from './CaregoryCard.module.css';
+import { MEDIA_BASE_URL } from '../../constants';
 
 const CategoryCard = ({ category }) => {
     const navigate = useNavigate();
@@ -13,9 +14,9 @@ const CategoryCard = ({ category }) => {
         <div
             className={`${classes.card_container} card mb-2`}
             onClick={handleClick}
-            // style={{
-            //     backgroundImage: `url(${category.image})`
-            // }}
+            style={{
+                backgroundImage: `url(${MEDIA_BASE_URL}${category.image})`
+            }}
         >
             <div className={classes.overlay_container}>
                 <h3 className={classes.card_title}>{category.name}</h3>
