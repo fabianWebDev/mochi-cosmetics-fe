01bd869179bf6ee46e5777777c8f4c6ee4758416
fabianWebDev <fabiancava22@gmail.com>
2025-10-04 17:21:25 -0600

    BP commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -33,8 +33,3 @@ export const removeUser = () => {
 export const calculateTotal = (items) => {
   return items.reduce((total, item) => total + item.price * item.quantity, 0);
 };
-
-export const validateEmail = (email) => {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-};
