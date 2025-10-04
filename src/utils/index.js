fa5884294180e4d17ba0e69dccce89be766d17f2
fabianWebDev <fabiancava22@gmail.com>
2025-10-03 21:19:41 -0600

    BP commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -38,7 +38,3 @@ export const validateEmail = (email) => {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(email);
 };
-
-export const validatePassword = (password) => {
-  return password.length >= 8;
-}; 
