b8926368fcca9f903e8c1f661e8cefc68b366a47
fabianWebDev <fabiancava22@gmail.com>
2025-10-11 13:29:03 -0600

    BP commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,7 +8,3 @@ export const formatPrice = (price) => {
 export const getToken = () => {
   return localStorage.getItem('auth_token');
 };
-
-export const setToken = (token) => {
-  localStorage.setItem('auth_token', token);
-};
